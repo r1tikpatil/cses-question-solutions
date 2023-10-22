ab0c39f2bfc952a3b3a2cc78d0d352f6153259ba
r1tikpatil <ritikpatil566@gmail.com>
2023-10-22 10:58:03 +0530

    distinctNumbers

--- sorting and searching/distinctNumbers.cpp
+++ sorting and searching/distinctNumbers.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 void solve(vector<ll> &arr, ll n)
 {
-    unordered_map<ll, ll> m;
-    for (auto i : arr)
-        m[i]++;
-    cout << m.size() << endl;
+    sort(arr.begin(), arr.end());
+    ll count = 1;
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] != arr[i - 1])
+            count++;
+    }
+    cout << count << endl;
 }
 
 int main()
@@ -17,7 +21,7 @@ int main()
     vector<ll> arr(n);
     for (int i = 0; i < n; i++)
         cin >> arr[i];
-        
+
     solve(arr, n);
     return 0;
 }
